177c895f52a1cb848d159bc1717988a93cb0e9c2
Pedro Santana Mendes <pmendes09@hotmail.com>
2022-09-18 12:06:17 -0300

    Button classes

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -12,15 +12,15 @@ class Calculator extends Component {
     this.addDigit = this.addDigit.bind(this)
   }
   clearMemory() {
-
+    console.log('limpar')
   }
 
   setOperation (operation) {
-
+    console.log(operation)
   }
 
   addDigit(n) {
-    
+    console.log(n)
   }
   render() {
     // const addDigit = n => this.addDigit(n)
@@ -29,22 +29,22 @@ class Calculator extends Component {
       <div className="calculator">
         <Display value={0}></Display>
         <Button label="AC" click={ this.clearMemory} triple></Button>
-        <Button label="/" click={this.operation} operation></Button>
+        <Button label="/" click={this.setOperation} operation></Button>
         <Button label="7" click={this.addDigit}></Button>
         <Button label="8" click={this.addDigit}></Button>
         <Button label="9" click={this.addDigit}></Button>
-        <Button label="*" click={this.operation} operation></Button>
+        <Button label="*" click={this.setOperation} operation></Button>
         <Button label="4" click={this.addDigit}></Button>
         <Button label="5" click={this.addDigit}></Button>
         <Button label="6" click={this.addDigit}></Button>
-        <Button label="-" click={this.operation} operation></Button>
+        <Button label="-" click={this.setOperation} operation></Button>
         <Button label="1" click={this.addDigit}></Button>
         <Button label="2" click={this.addDigit}></Button>
         <Button label="3" click={this.addDigit}></Button>
-        <Button label="+" click={this.operation} operation></Button>
+        <Button label="+" click={this.setOperation} operation></Button>
         <Button label="0" click={this.addDigit} double></Button>
         <Button label="." click={this.operation}></Button>
-        <Button label="=" click={this.operation} operation></Button>
+        <Button label="=" click={this.setOperation} operation></Button>
       </div>
     );
   }
