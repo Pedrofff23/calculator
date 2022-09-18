7a62c132d79fd40bd4c7020d6f5017008047bb5c
Pedro Santana Mendes <pmendes09@hotmail.com>
2022-09-18 10:39:42 -0300

    Body of the Calculator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "./Calculator.css";
+
+//cpc
+class Calculator extends Component {
+  state = {};
+  render() {
+    return <div className="calculator"></div>;
+  }
+}
+
+export default Calculator;
